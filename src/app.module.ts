7441df842d950bf39c3f8eaa8429ba3892add027
punnreaykhy <punnreaykhy1010@gmail.com>
2023-08-21 10:03:51 +0700

    removed modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,19 +9,16 @@ import { RolePermissionModule } from './modules/role_permission/role_permission.
 import { PermissionsModule } from './modules/permissions/permissions.module';
 import { TopicsModule } from './modules/topics/topics.module';
 import { TopicContentsModule } from './modules/topic_contents/topic_contents.module';
-import { MessagesModule } from './modules/messages/messages.module';
-import { AttachmentsModule } from './modules/attachments/attachments.module';
 import { UserTagModule } from './modules/user_tag/user_tag.module';
 import { TagsModule } from './modules/tags/tags.module';
 import { TagTopicModule } from './modules/tag_topic/tag_topic.module';
 import { ChatService } from './modules/chat/services/chat.service';
-import { UserToTopicModule } from './src/modules/user_to_topic/user_to_topic.module';
 import { UserToTopicModule } from './modules/user_to_topic/user_to_topic.module';
 
 
 @Module({
   imports: [TypeOrmModule.forRoot(databaseConfig), // Connect to the database
-  ChatModule, UsersModule, RolesModule, RolePermissionModule, PermissionsModule, TopicsModule, TopicContentsModule, MessagesModule, AttachmentsModule, UserTagModule, TagsModule, TagTopicModule, UserToTopicModule],
+  ChatModule, UsersModule, RolesModule, RolePermissionModule, PermissionsModule, TopicsModule, TopicContentsModule, UserTagModule, TagsModule, TagTopicModule, UserToTopicModule],
   controllers: [],
   providers: [ChatService],
 })
