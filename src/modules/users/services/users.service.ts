680221ac758e976006cfabd7db475f1f37a72891
PEOUSOKLEANG <sokleang34802@gmail.com>
2023-08-22 15:38:28 +0700

    auth

--- src/modules/users/services/users.service.ts
+++ src/modules/users/services/users.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from '../dto/create-user.dto';
+// import { CreateUserDto } from '../dto/create-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from '../entities/user.entity';
+import { CreaetUserType } from 'src/utils/types';
 
 
 @Injectable()
@@ -10,15 +11,14 @@ export class UsersService {
   constructor(@InjectRepository(User)private usersRepositiry:Repository<User>){
 
   }
-  async createUser(createUserDto:CreateUserDto): Promise<User>{
-    const user = await this.usersRepositiry.create(createUserDto);
+  async createUser(createUserType:CreaetUserType): Promise<User>{
+    const user = await this.usersRepositiry.create(createUserType);
     await user.save();
 
     delete user.password;
     return user;
-
-
   }
+  
   async findUserByEmail(email: string){
     return await User.findOne({
       where: {
